f587f4fb0ccb39676eeace2071679cafd807196c
Yash Chauhan <chauhanyash000@gmail.com>
2024-11-06 02:28:07 +0530

    Create page.tsx

---
+++ src/app/page.tsx
@@ -0,0 +1,9 @@
+import DashboardSection from '@/components/DashboardSection'
+
+export default function Home() {
+  return (
+    <main className="min-h-screen">
+      <DashboardSection />
+    </main>
+  )
+}
