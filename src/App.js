229969707405bb6ff70d76222719a9a45f8de9af
Yash Chauhan <chauhanyash000@gmail.com>
2024-11-05 18:54:52 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import AboutSection from './components/sections/AboutSection';
 import SecuritySection from './components/sections/SecuritySection';
 import WaitlistSection from './components/sections/WaitlistSection';
 import { themeColors, typography } from './styles/theme';
+import DashboardSection from './components/sections/DashboardSection';
 
 const App = () => {
   const [activeSection, setActiveSection] = useState('home');
@@ -20,7 +21,7 @@ const App = () => {
 
   useEffect(() => {
     const handleScroll = () => {
-      const sections = ['home', 'products', 'about', 'security', 'waitlist'];
+      const sections = ['home', 'products', 'about','dashboard', 'security', 'waitlist'];
       const current = sections.find(section => {
         const element = document.getElementById(section);
         if (element) {
@@ -63,6 +64,10 @@ const App = () => {
       
       <div id="waitlist">
         <WaitlistSection />
+      </div>
+
+      <div id="dashboard">
+        <DashboardSection />
       </div>
 
       <Footer />
