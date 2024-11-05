989d4a2e650cd99223584bc2a1d2d01245219844
Yash Chauhan <chauhanyash000@gmail.com>
2024-11-06 03:49:15 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,7 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',
+  images: {
+    unoptimized: true,
+  },
   reactStrictMode: true,
   swcMinify: true,
+  trailingSlash: true,
 }
 
 module.exports = nextConfig
